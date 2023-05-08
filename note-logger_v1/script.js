74f0adb69779de4a0d0cd11dc4124d709bea9dfc
Pratik Jindal <117007380+jindalpratik@users.noreply.github.com>
2023-05-08 18:59:00 +0530

    Remove example notes and lint. (#21)
    * remove example notes
    
    * lint

--- note-logger_v1/script.js
+++ note-logger_v1/script.js
@@ -1,13 +1,13 @@
-const noteForm = document.querySelector('#note-form');
-const noteInput = document.querySelector('#note-input');
-const noteList = document.querySelector('#note-list');
+const noteForm = document.querySelector("#note-form");
+const noteInput = document.querySelector("#note-input");
+const noteList = document.querySelector("#note-list");
 
 let notes = [];
 
 function addNote() {
   // Get note text
   const text = noteInput.value.trim();
-  if (text === '') {
+  if (text === "") {
     return;
   }
 
@@ -15,22 +15,21 @@ function addNote() {
   notes.push(text);
 
   // Add note to list
-  const li = document.createElement('li');
+  const li = document.createElement("li");
   li.textContent = text;
 
-
   // Creating the elements and their classes.
-  const div = document.createElement('div');
-  div.setAttribute("class","note-buttons");
-  const editButton = document.createElement('button');
-  editButton.setAttribute("class","edit-button");
-  const deleteButton = document.createElement('button');
-  deleteButton.setAttribute("class","delete-button");
-  const editButtonIcon = document.createElement('i');
-  editButtonIcon.setAttribute("class","fas fa-edit");
-  const deleteButtonIcon = document.createElement('i');
-  deleteButtonIcon.setAttribute("class","fas fa-trash-alt");
-  
+  const div = document.createElement("div");
+  div.setAttribute("class", "note-buttons");
+  const editButton = document.createElement("button");
+  editButton.setAttribute("class", "edit-button");
+  const deleteButton = document.createElement("button");
+  deleteButton.setAttribute("class", "delete-button");
+  const editButtonIcon = document.createElement("i");
+  editButtonIcon.setAttribute("class", "fas fa-edit");
+  const deleteButtonIcon = document.createElement("i");
+  deleteButtonIcon.setAttribute("class", "fas fa-trash-alt");
+
   // Adding the note to the html with the buttons included.
   editButton.appendChild(editButtonIcon);
   deleteButton.appendChild(deleteButtonIcon);
@@ -40,10 +39,10 @@ function addNote() {
   noteList.appendChild(li);
 
   // Clear input
-  noteInput.value = '';
+  noteInput.value = "";
 }
 
-noteForm.addEventListener('submit', function(event) {
+noteForm.addEventListener("submit", function (event) {
   event.preventDefault();
   addNote();
 });
