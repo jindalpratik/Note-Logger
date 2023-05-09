c901de4b24a7d0ac96b13e344d5e7e83810fc0b5
Pratik Jindal <117007380+jindalpratik@users.noreply.github.com>
2023-05-09 19:07:34 +0530

    Functioning delete button in new notes
    Also converted the notes list to an object

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const noteForm = document.querySelector("#note-form");
 const noteInput = document.querySelector("#note-input");
 const noteList = document.querySelector("#note-list");
 
-let notes = [];
+let notes = {};
+let id = 0;
 
 function addNote() {
   // Get note text
@@ -11,11 +12,15 @@ function addNote() {
     return;
   }
 
-  // Add note to array
-  notes.push(text);
+  //Increment ID
+  id += 1;
+
+  // Add note to dictionary
+  notes[id] = text;
 
   // Add note to list
   const li = document.createElement("li");
+  li.setAttribute("id","parent-"+id);
   li.textContent = text;
 
   // Creating the elements and their classes.
@@ -25,6 +30,9 @@ function addNote() {
   editButton.setAttribute("class", "edit-button");
   const deleteButton = document.createElement("button");
   deleteButton.setAttribute("class", "delete-button");
+  deleteButton.setAttribute("id",id);
+  deleteButton.setAttribute("onclick","delete_note(id);");
+
   const editButtonIcon = document.createElement("i");
   editButtonIcon.setAttribute("class", "fas fa-edit");
   const deleteButtonIcon = document.createElement("i");
@@ -47,8 +55,10 @@ noteForm.addEventListener("submit", function (event) {
   addNote();
 });
 
-function delete_li(id) {
-  parentId = "parent-"+id
+// Delete note.
+function delete_note(del_id) {
+  parentId = "parent-"+del_id
   const li = document.getElementById(parentId);
   li.remove();  
+  delete notes[del_id];
 }
