821563ae1bf0c7772ef9d92e1585f1ed42adcc19
Pratik Jindal <117007380+jindalpratik@users.noreply.github.com>
2023-05-09 21:29:08 +0530

    Implement preservation of notes between browser refreshes. (#28)
    Closes #4 .
    
    * initial implementation for preserving between browser refreshes
    
    * Add back the notes after browser refresh
    
    - Notes survive browser refresh
    - Pulled the code which added the notes into a seperate function
    
    * lint
    
    * increment verson

--- script.js
+++ script.js
@@ -5,34 +5,32 @@ const noteList = document.querySelector("#note-list");
 let notes = {};
 let id = 0;
 
-function addNote() {
-  // Get note text
-  const text = noteInput.value.trim();
-  if (text === "") {
-    return;
-  }
+if (!(localStorage.getItem("notes") === null)) {
+  notes = JSON.parse(localStorage.getItem("notes"));
+  id = Number(localStorage.getItem("id"));
+}
 
-  //Increment ID
-  id += 1;
+// Add notes after browser refresh.
+for (curId in notes) {
+  addNoteToUl(curId);
+}
 
-  // Add note to dictionary
-  notes[id] = text;
-
-  // Add note to list
+function addNoteToUl(curId) {
   const li = document.createElement("li");
-  li.setAttribute("id", "parent-" + id);
-  li.textContent = text;
+  li.setAttribute("id", "parent-" + curId);
+  li.textContent = notes[curId];
 
-  // Creating the elements and their classes.
+  // Creating the Buttons and their classes.
   const div = document.createElement("div");
   div.setAttribute("class", "note-buttons");
   const editButton = document.createElement("button");
   editButton.setAttribute("class", "edit-button");
   const deleteButton = document.createElement("button");
   deleteButton.setAttribute("class", "delete-button");
-  deleteButton.setAttribute("id", id);
+  deleteButton.setAttribute("id", curId);
   deleteButton.setAttribute("onclick", "delete_note(id);");
 
+  // Creating the Button icons and their classes.
   const editButtonIcon = document.createElement("i");
   editButtonIcon.setAttribute("class", "fas fa-edit");
   const deleteButtonIcon = document.createElement("i");
@@ -50,6 +48,26 @@ function addNote() {
   noteInput.value = "";
 }
 
+function addNote() {
+  // Get note text
+  const text = noteInput.value.trim();
+  if (text === "") {
+    return;
+  }
+
+  //Increment ID
+  id += 1;
+
+  // Add note to dictionary
+  notes[id] = text;
+
+  addNoteToUl(id);
+
+  //Store the notes in browser storage
+  localStorage.setItem("notes", JSON.stringify(notes));
+  localStorage.setItem("id", id);
+}
+
 noteForm.addEventListener("submit", function (event) {
   event.preventDefault();
   addNote();
@@ -61,4 +79,6 @@ function delete_note(del_id) {
   const li = document.getElementById(parentId);
   li.remove();
   delete notes[del_id];
+
+  localStorage.setItem("notes", JSON.stringify(notes));
 }
